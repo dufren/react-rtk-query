67fdf2a8901ac36f8bc8bd7c025e21ab0c57d7a7
Özgür ÇOKÇEKEN <ozgur.cokceken@istinye.edu.tr>
2023-02-03 16:56:59 +0300

    commit

--- src/features/admin/AdminDashboard.js
+++ src/features/admin/AdminDashboard.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AdminPanel = () => {
   return (
-    <div>Dashboard</div>
+    <div className='p-5'>
+      <h1 className='text-3xl text-gray-600 font-medium'>Welcome to the RYE-G</h1>
+      <p className='mt-10 w-2/5 text-sm font-medium text-gray-600'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut esse reiciendis facere cum sapiente voluptatibus asperiores voluptatem nisi blanditiis impedit explicabo, officiis accusantium aperiam non?</p>
+    </div>
   )
 }
 
